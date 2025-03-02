d342f5231a6f85c692762f182de22f40758a472d
yadavjaimin <your-email@example.com>
2025-03-02 13:08:52 +0530

    add new file git practice

---
+++ resources/views/git_practice.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>heeloits git practice </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi odit neque saepe quod ipsa, a, commodi atque amet impedit nisi veritatis consequatur molestias corporis eius magni facere deleniti. Maxime, delectus.</p>
+</body>
+</html>
